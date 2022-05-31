69e1452a3daec818ebb4e91751a9d57f9c02ea3f
Stefano Butori <stefano.butori@sou.inteli.edu.br>
2022-05-31 15:23:25 -0300

    Implementar algumas validações no controller de assignments

--- src/controllers/assignmentController.js
+++ src/controllers/assignmentController.js
@@ -1,18 +1,45 @@
 const Assignment = require('../models').Assignment;
+const Employee = require('../models').Employee;
+const Project = require('../models').Project;
 
 exports.createAssignment = async (req, res) => {
-  await Assignment.create(req.body);
-  res.send('Alocação criada com sucesso!');
+  // verificar se o projeto existe
+  const project = await Project.findByPk(req.body.projectId);
+  // verificar se o funcionario existe
+  const employee = await Employee.findByPk(req.body.employeeId);
+  if (project && employee) {
+    await Assignment.create(req.body);
+    res.send('Alocação cadastrada com sucesso!');
+  } else {
+    res.status(404).send('Projeto ou funcionário não encontrado!');
+  }
 };
 
 exports.updateAssignment = async (req, res) => {
   const assignment = await Assignment.findByPk(req.params.id);
 
+  // verificar se o projeto existe
+  if (req.body.projectId) {
+    const project = await Project.findByPk(req.body.projectId);
+    if (!project) {
+      res.status(404).send('Projeto não encontrado!');
+      return;
+    }
+  }
+  // verificar se o funcionario existe
+  if (req.body.employeeId) {
+    const employee = await Employee.findByPk(req.body.employeeId);
+    if (!employee) {
+      res.status(404).send('Funcionário não encontrado!');
+      return;
+    }
+  }
+
   if (assignment) {
     await assignment.update(req.body);
     res.send('Alocação atualizada com sucesso!');
   } else {
-    res.status(404).send('Alocação não encontrada!');
+    res.status(404).send('Alocação, projeto ou funcionário não encontrado!');
   }
 };
 
