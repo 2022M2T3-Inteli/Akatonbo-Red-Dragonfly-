f40093aed0e4a10363d17f7d44f0d4c6ef429333
Stefano Butori <stefano.butori@sou.inteli.edu.br>
2022-06-02 00:14:55 -0300

    Alimentar a tabela do dashboard com dados do backend

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,7 +1,24 @@
-exports.getTable = function (req, res) {
-  res.render('pages/dashboard/table');
+const Assignment = require('../models').Assignment;
+const Role = require('../models').Role;
+const Employee = require('../models').Employee;
+
+exports.getTable = async (req, res) => {
+  const roles = await Role.findAll({
+    include: [
+      {
+        model: Employee,
+        include: [
+          {
+            model: Assignment,
+          },
+        ],
+      },
+    ],
+  });
+
+  res.render('pages/dashboard/table', { roles });
 };
 
-exports.getChart = function (req, res) {
+exports.getChart = async (req, res) => {
   res.render('pages/dashboard/chart');
 };
